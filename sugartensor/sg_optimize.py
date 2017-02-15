a8d861a0bcfe7d51ada886a2cbe604eb630b49d1
Brett Beaulieu-Jones <brettbj@gmail.com>
2017-02-15 18:53:14 -0500

    update dp implementation

--- sugartensor/sg_optimize.py
+++ sugartensor/sg_optimize.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import tensorflow as tf
 
-import dp.per_example_gradients
+import sugartensor.dp.per_example_gradients
 
 
 class DPGradientDescentOptimizer(tf.train.GradientDescentOptimizer):
