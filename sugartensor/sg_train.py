9f2f7a28ba532db86e3b777f89aaed23185e38b1
Brett Beaulieu-Jones <brettbj@gmail.com>
2017-02-22 21:25:12 -0500

    update optimize

--- sugartensor/sg_train.py
+++ sugartensor/sg_train.py
@@ -177,7 +177,7 @@ def sg_optim(loss, **kwargs):
         optim = tf.sg_optimize.AdaMaxOptimizer(learning_rate=opt.lr, beta1=opt.beta1, beta2=opt.beta2)
     elif opt.optim == 'Adam':
         optim = tf.train.AdamOptimizer(learning_rate=opt.lr, beta1=opt.beta1, beta2=opt.beta2)
-    elif: opt.optim == 'DP_GD':
+    elif opt.optim == 'DP_GD':
         optim = tf.sg_optimize.DPGradientDescentOptimizer(
                     opt.lr,
                     [opt.eps, opt.delta],
